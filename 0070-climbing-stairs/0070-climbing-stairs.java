3c7feca7ea8e9664e8d887b2e271cbaa45c80507
Aditya Jha <jhaa27211@gmail.com>
2026-09-03 22:50:16 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    public int climbStairs(int n) {
+        if(map.containsKey(n)) {
+            return map.get(n);
+        }
+        if(n == 0) {
+            return 1;
+        } else if(n == -1) {
+            return 0;
+        }
+
+        int res = climbStairs(n-1) + climbStairs(n-2);
+        map.put(n, res);
+        return res;
+    }
+
+}
